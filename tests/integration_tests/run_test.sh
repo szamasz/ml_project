8fdb38e41dabab6ab5e4affd982d813ec8641734
szamasz <szamasz1112@gmail.com>
2024-10-26 13:02:50 +0200

    before linting

--- tests/integration_tests/run_test.sh
+++ tests/integration_tests/run_test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 FULL_PATH_TO_SCRIPT="$(realpath "${BASH_SOURCE}")"
 DIRNAME="$(dirname "$FULL_PATH_TO_SCRIPT")"
@@ -15,11 +15,15 @@ docker compose -f $ROOT_DIR/docker/docker-compose.yml -p $ENV_VAR --profile $ENV
 echo "MLFLOW_TRACKING_URI" $MLFLOW_TRACKING_URI
 echo "OPTUNA_DB_URI" $OPTUNA_DB_URI
 
-$PYTHON_BIN -m mlproject --config_file=apartments_selected_columns.yml --experiment_name=apartments_selected_columns_linear --number_of_trials=1 --sampler=Random
+$PYTHON_BIN -m mlproject --config_file=apartments_selected_columns.yml --experiment_name=apartments_selected_columns_linear --number_of_trials=10 --sampler=Random
 
 $PYTHON_BIN $TEST_DIR/test_model.py
 
-#docker compose -f $ROOT_DIR/docker/docker-compose.yml -p $ENV_VAR --profile $ENV_VAR --env-file $ROOT_DIR/conf/env/env_docker_$ENV_VAR down
+
+docker compose -f $ROOT_DIR/docker/docker-compose.yml -p $ENV_VAR --profile $ENV_VAR --env-file $ROOT_DIR/conf/env/env_docker_$ENV_VAR down
+echo "Integration test services stopped"
+
+sudo rm -rf $TEST_DIR/storage-tests
+echo "Deleted: $TEST_DIR/storage-tests"
 
 echo "Integration tests FINISHED"
-#rm storage local
