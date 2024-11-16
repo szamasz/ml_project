f120220b2986ecbae7a9309b928aa31f609424ed
szamasz <29711821+szamasz@users.noreply.github.com>
2024-11-16 12:02:11 +0100

    fixed data source directories (#2)

--- src/mlproject/etl_data.py
+++ src/mlproject/etl_data.py
@@ -109,4 +109,4 @@ def process_data():
 
     ApartmentsSchema.validate(df_apartments_1)
 
-    df_apartments_1.to_csv("data/apartments.csv", index=False)
+    df_apartments_1.to_csv("data/05_model_input/apartments.csv", index=False)
